f47a64690e4fa5c5016dfafede3042c1372fc741
Michael Bradt, Maly Phongsavanh and Victoria Martinez <maly@>
2018-10-15 16:47:33 -0700

    add many variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,21 @@ var reversed = function(lastLetter, firstLetter) {
   return lastLetter + firstLetter;
 };
 
+
 $(document).ready(function() {
   var inputSentence = prompt("Enter a sentence?");
   var upper = inputSentence.toUpperCase().charAt(0);
   var last = inputSentence.toUpperCase().charAt(inputSentence.length - 1);
   var reversing = reversed(last, upper);
   var unreversing = reversing.split("").reverse().join("");
-
   var concatening = inputSentence.concat(reversing);
+  var counting = parseInt(inputSentence.length/2);
+  var replaceCount = inputSentence.charAt(counting);
+  var actuallyReplace = replaceCount.concat.charAt(counting);
 
-  alert(reversing);
-  alert(unreversing);
-  alert(concatening);
+
+
+  alert(actuallyReplace);
+
 
 });
